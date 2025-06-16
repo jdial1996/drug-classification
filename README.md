5352f5d53b11411b46b7e2135abac8e52b1311df
Jasdeep Singh <jsd@Jasdeeps-MacBook-Pro.local>
2025-06-16 16:32:42 +0100

    black linting

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ This project solves a drug classification problem.  It serves as a demo project 
 - ./datasets for all data files 
 - ./models for trained model files 
 - ./results for saving metrics and results in PNG files
+- train.py contains a Standardised script that runs everytime in our pipeline when there is a change in the data or code
 
 ## Hugging Face Spaces
 - Using Hugging Face Spaces as a deployment target as it's easy to build and deploy machine learning demo apps. 
--- train.py
+++ train.py
@@ -1,41 +1,48 @@
-# Standardised script that runs everytime in our pipeline when there is a change in the data or code
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer # lets us link different pipelines together 
-# Encoder is to used to encode categorical data 
-from sklearn.preprocessing import StandardScaler, OrdinalEncoder# preproccessing = cleaning the data / preparing it for the model 
-from sklearn.impute import SimpleImputer # handle missing data 
-from sklearn.ensemble import RandomForestClassifier # the models 
-from sklearn.metrics import accuracy_score, f1_score # test/evaluate model 
+from sklearn.compose import (
+    ColumnTransformer,
+)  # lets us link different pipelines together
+
+# Encoder is to used to encode categorical data
+from sklearn.preprocessing import (
+    StandardScaler,
+    OrdinalEncoder,
+)  # preproccessing = cleaning the data / preparing it for the model
+from sklearn.impute import SimpleImputer  # handle missing data
+from sklearn.ensemble import RandomForestClassifier  # the models
+from sklearn.metrics import accuracy_score, f1_score  # test/evaluate model
 import skops.io as skops
 
 
 # Load Data
-drug_df = pd.read_csv('./datasets/drug.csv')
+drug_df = pd.read_csv("./datasets/drug.csv")
 drug_df.info()
 
-# Shuffle Data 
+# Shuffle Data
 
 drug_df = drug_df.sample(frac=1)
 
-# # Print Top 3 rows 
+# # Print Top 3 rows
 # # print(drug_df.head(3))
 
 # # Independent variable
 X = drug_df.drop("Drug", axis=1).values
 
-# # Dependent variable 
+# # Dependent variable
 Y = drug_df.Drug.values
 
 # # This will return 4 datasets.  X_train (70% of X), X_test (30% of X) Y_train (70% of Y), Y_test (30% of Y)
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.3, random_state= 125)
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=125
+)
 
-numerical_columns = [0,4]
-categorical_columns = [1,2,3]
+numerical_columns = [0, 4]
+categorical_columns = [1, 2, 3]
 
 
-# Use pipeline to apply sequential steps. Use column transformer to apply steps in parallel 
+# Use pipeline to apply sequential steps. Use column transformer to apply steps in parallel
 
 transform = ColumnTransformer(
     [
@@ -45,16 +52,21 @@ transform = ColumnTransformer(
     ]
 )
 
-pipeline = Pipeline([
-    ('preprocessor', transform),
-    ('model', RandomForestClassifier(n_estimators=100, random_state=125)) # data will end up here
-])
+pipeline = Pipeline(
+    [
+        ("preprocessor", transform),
+        (
+            "model",
+            RandomForestClassifier(n_estimators=100, random_state=125),
+        ),  # data will end up here
+    ]
+)
 
 pipeline.fit(X_train, Y_train)
 
 predictions = pipeline.predict(X_test)
 
-accuracy = accuracy_score(Y_test,predictions)
+accuracy = accuracy_score(Y_test, predictions)
 f1 = f1_score(Y_test, predictions, average="macro")
 
 print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
